aa855a62a3a07370f784e170ad7f6dc2878c6ce1
Vinny Tiwari <tiwari.vineet@outlook.com>
2015-05-05 16:27:08 -0700

    initial and final commit

--- Box/main.c
+++ Box/main.c
@@ -8,8 +8,60 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-  // insert code here...
-  printf("Hello, World!\n");
-    return 0;
+typedef struct {
+
+  float length;
+  float width;
+  float depth;
+
+} Box;
+
+float boxVolume(Box firstBox);
+
+Box makeABox(float length, float width, float depth);
+
+float calcVolumeDifference( Box box1, Box box2);
+
+
+int main() {
+
+
+  Box thisBox1 = makeABox(4, 5, 6);
+  Box thisBox2 = makeABox(7, 8, 9);
+  calcVolumeDifference (thisBox1, thisBox1);
+
+  printf("volume of the first box is: %f \n", boxVolume(thisBox1));
+  printf("volume of the second box is: %f \n", boxVolume(thisBox2));
+  printf("The difference between the volumes of the two boxes is: %f \n", calcVolumeDifference(thisBox1, thisBox2));
+
+  return 0;
 }
+
+float boxVolume(Box specialBox) {
+
+ return specialBox.length * specialBox.width * specialBox.depth;
+}
+
+Box makeABox(float length, float width, float depth) {
+
+  Box myBox = { .length = length, .width = width, .depth = depth };
+  return myBox;
+}
+
+float calcVolumeDifference( Box box1, Box box2){
+
+  return (boxVolume(box1) / boxVolume (box2));
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
